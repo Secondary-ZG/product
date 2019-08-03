1470b4cacfd83c738ba3b8938118d5e7dee54434
zhangzhengguo <zzg@hugehuge.cn>
2019-08-03 16:03:57 +0800

    商品服务项目初始化

---
+++ src/main/java/com/imooc/product/controller/ProductController.java
@@ -0,0 +1,21 @@
+package com.imooc.product.controller;
+
+/**
+ * Java Class
+ *
+ * @author Secondary
+ * @date 2019-07-30 10:59
+ * Description: 商品控制层
+ */
+public class ProductController {
+
+    /**
+     * 1.查询所有在在架的商品
+     * 2.获取类目type列表
+     * 3.查询类目
+     * 4.构造数据
+     */
+    public void list(){
+
+    }
+}
