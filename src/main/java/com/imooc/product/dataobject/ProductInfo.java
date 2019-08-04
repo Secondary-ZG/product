adef97dc35c23c4d308d40fb806eda7da87b849d
Secondary-ZG <17764540313@163.com>
2019-08-04 13:07:48 +0800

    添加商品信息实体类

---
+++ src/main/java/com/imooc/product/dataobject/ProductInfo.java
@@ -0,0 +1,127 @@
+package com.imooc.product.dataobject;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * Java Class
+ * Created By Secondary
+ * On 2019/8/4 12:53
+ * Description: 商品信息表
+ */
+@Entity
+public class ProductInfo {
+
+    /** 商品编号 */
+    @Id
+    private String productId;
+
+    /** 商品名称 */
+    private String productName;
+
+    /** 商品单价 */
+    private BigDecimal productPrice;
+
+    /** 商品库存 */
+    private Integer productStock;
+
+    /** 商品描述 */
+    private String productDescription;
+
+    /** 商品图片 */
+    private String productIcon;
+
+    /** 商品状态：0正常,1下架 */
+    private Integer productStatus;
+
+    /** 类目编号 */
+    private Integer categoryType;
+
+    /** 创建时间 */
+    private Date createTime;
+
+    /** 更新时间 */
+    private Date updateTime;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public BigDecimal getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(BigDecimal productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public Integer getProductStock() {
+        return productStock;
+    }
+
+    public void setProductStock(Integer productStock) {
+        this.productStock = productStock;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getProductIcon() {
+        return productIcon;
+    }
+
+    public void setProductIcon(String productIcon) {
+        this.productIcon = productIcon;
+    }
+
+    public Integer getProductStatus() {
+        return productStatus;
+    }
+
+    public void setProductStatus(Integer productStatus) {
+        this.productStatus = productStatus;
+    }
+
+    public Integer getCategoryType() {
+        return categoryType;
+    }
+
+    public void setCategoryType(Integer categoryType) {
+        this.categoryType = categoryType;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
